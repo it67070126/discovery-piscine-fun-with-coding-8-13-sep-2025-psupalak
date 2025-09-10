01af92812f9f98e910db643d270e4b623af43c8b
Pattaradanai <67070126@kmitl.ac.th>
2025-09-11 02:52:32 +0700

    Update main.js

--- cell03/ex01/main.js
+++ cell03/ex01/main.js
@@ -6,13 +6,11 @@ const maxSize = 420;
 const colors = ['red', 'green', 'blue'];
 let colorIndex = 0;
 
-// Handle click: grow size + change color
 balloon.addEventListener('click', () => {
     size += 10;
-    colorIndex = (colorIndex + 1) % colors.length;
+    colorIndex = (colorIndex + 1)  % colors.length;
 
     if (size > maxSize) {
-        // Explode and reset
         size = minSize;
         colorIndex = 0;
     }
@@ -20,18 +18,16 @@ balloon.addEventListener('click', () => {
     updateBalloon();
 });
 
-// Handle mouse leave: shrink size + reverse color
 balloon.addEventListener('mouseleave', () => {
     size -= 5;
-    if (size < minSize) size = minSize;
+    if (size < minSize)
+        size = minSize;
 
-    // Reverse color order
     colorIndex = (colorIndex - 1 + colors.length) % colors.length;
 
     updateBalloon();
 });
 
-// Update balloon style
 function updateBalloon() {
     balloon.style.width = size + 'px';
     balloon.style.height = size + 'px';
